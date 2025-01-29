5a46808f0a62001887e86fe1752a7835290a9a22
bethmannRolf <devak.robe@gmail.com>
2025-01-29 07:58:43 +0100

    added shared folder and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { GameComponent } from './game/game.component';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 export const routes: Routes = [
     {path: '', component: StartScreenComponent },
-    {path: 'game', component: GameComponent }
+    {path: 'game', component: GameComponent },
+    {path:'footer', component: FooterComponent}
 ];
