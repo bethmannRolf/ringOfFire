6cdad044e9e3bc624b86598505864da453c95108
bethmannRolf <devak.robe@gmail.com>
2025-01-30 11:19:27 +0100

    implemented game route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { GameComponent } from './game/game.component';
-import { FooterComponent } from "./shared/components/footer/footer.component";
+
 
 export const routes: Routes = [
     {path: '', component: StartScreenComponent },
-    {path: 'game', component: GameComponent },
-    {path:'footer', component: FooterComponent}
+    {path: 'game', component: GameComponent }
+   
 ];
