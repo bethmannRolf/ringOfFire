5ad4c1dc8ab4b113b612ce4417c81a55b50fffc1
bethmannRolf <devak.robe@gmail.com>
2025-02-05 15:49:04 +0100

    added add player component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,58 +2,78 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../game';
 import { PlayerComponent } from "../player/player.component";
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, MatDialogModule, MatInputModule],
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit{
-pickCardAnimation = false;
-game!: Game;
-currentCard: string = '';
-constructor(){
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game!: Game;
+  currentCard: string = '';
+  constructor(public dialog: MatDialog) {
 
-}
-
-ngOnInit():void{
-this.newGame();
-}
-
-newGame(){
-  this.game = new Game();
-  console.log(this.game)
-}
-
-
-
-
-takeCard() {
-  if (!this.pickCardAnimation) {
-    
-  
-  const card = this.game.stack.pop();
-  if (card !== undefined) {
-    this.currentCard = card;
-    console.log(this.currentCard)
-    this.pickCardAnimation = true;
-    // this.game.playedCards.push(this.currentCard);
-
-setTimeout(() => {
-  this.game.playedCards.push(this.currentCard);
-  this.pickCardAnimation = false;
-}, 1000);
-
-  } else {
-    console.warn('Keine Karten mehr im Stapel!');
   }
-}
-}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game)
+  }
+
+
+
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+
+
+      const card = this.game.stack.pop();
+      if (card !== undefined) {
+        this.currentCard = card;
+        console.log(this.currentCard)
+        this.pickCardAnimation = true;
+        // this.game.playedCards.push(this.currentCard);
+
+        setTimeout(() => {
+          this.game.playedCards.push(this.currentCard);
+          this.pickCardAnimation = false;
+        }, 1000);
+
+      } else {
+        console.warn('Keine Karten mehr im Stapel!');
+      }
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: { name: this.name(), animal: this.animal() },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if (result !== undefined) {
+        this.animal.set(result);
+      }
+    });
+  }
+
+
 
 
 
