bec417acd42042b7dbc3f49fbf66623e79234ffd
bethmannRolf <devak.robe@gmail.com>
2025-02-06 07:06:02 +0100

    dinamic adding of players

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    players: string[] = ['Hans', 'Peter', 'Hunold'];
+    players: string[] = [];
     stack: string[] = [];
     playedCards: string[] = [];
     currentPlayer: number = 0;
